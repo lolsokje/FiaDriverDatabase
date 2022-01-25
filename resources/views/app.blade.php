1d84de8d95284c555521b595122a4ea79e97ec8e
Alex van Daalen <alex9521@hotmail.com>
2022-01-25 15:05:38 +0100

    App name

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>{{ config('app.name') }}</title>
     <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
     <script src="{{ mix('/js/app.js') }}" defer></script>
     @inertiaHead
