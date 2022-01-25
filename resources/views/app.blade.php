d88fa601a03beaefacec9929a59ffd6f4216819f
Alex van Daalen <alex9521@hotmail.com>
2022-01-25 13:24:50 +0100

    Add Bootstrap 5

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Laravel</title>
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
     <script src="{{ mix('/js/app.js') }}" defer></script>
     @inertiaHead
 </head>
