daf40454edd5740c0f47dd2149211e845bd277a4
Alex <alex9521@hotmail.com>
2023-07-17 12:55:51 +0200

    Fixed index page sorting

--- app/Http/Controllers/Actions/ShowIndexPageController.php
+++ app/Http/Controllers/Actions/ShowIndexPageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Actions;
 
+use App\Actions\Drivers\SortBySeriesAndTeamName;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Admin\Drivers\DetailedDriverResource;
 use App\Http\Resources\Admin\Series\BaseSeriesResource;
@@ -13,10 +14,18 @@ use Inertia\Response;
 
 class ShowIndexPageController extends Controller
 {
-    public function __invoke(Request $request): Response
+    public function __invoke(Request $request, SortBySeriesAndTeamName $sortBySeriesAndTeamName): Response
     {
         $drivers = Driver::query()
-            ->sortedBySeries();
+            ->with([
+                'team' => ['owner'],
+                'series',
+            ])
+            ->get()
+            ->sort(fn (
+                Driver $driverOne,
+                Driver $driverTwo,
+            ) => $sortBySeriesAndTeamName->handle($driverOne, $driverTwo));
 
         return Inertia::render('Index', [
             'drivers' => DetailedDriverResource::collection($drivers),
